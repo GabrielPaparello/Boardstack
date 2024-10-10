cd7f33811f3aaee1bfe4b494bf60f1dc245e2937
GabrielPaparello <69154223+GabrielPaparello@users.noreply.github.com>
2024-10-10 16:39:19 +0000

    pending push of hero

--- boardstack/src/app/page.tsx
+++ boardstack/src/app/page.tsx
@@ -1,9 +1,15 @@
 import { Nav } from "@/ui/components/Nav/Nav";
+import { Hero } from "@/ui/components/sections/hero/Hero";
 
 export default function Home() {
   return (
     <div className="bg-light-primary dark:bg-dark-primary flex flex-col ">
-      <Nav />
+      <header>
+        <Nav />
+      </header>
+      <section>
+        <Hero />
+      </section>
     </div>
   );
 }
---
+++ boardstack/src/ui/components/sections/hero/Hero.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Hero = () => {
+  return (
+    <section>
+      <article>
+        <h1> la Sobrecarga de tareas</h1>
+      </article>
+      <article></article>
+      <article></article>
+    </section>
+  );
+};
