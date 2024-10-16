69a732b24f02517acaa86f33d874dd3f0527aacd
GabrielPaparello <69154223+GabrielPaparello@users.noreply.github.com>
2024-10-16 16:50:55 +0000

    finished by now the hero, just gotta fixed animations strokes and colores for multiples colorthemes

--- boardstack/src/ui/components/sections/hero/CardDesktop.tsx
+++ boardstack/src/ui/components/sections/hero/CardDesktop.tsx
@@ -1,52 +1,66 @@
 "use client";
 import { Player } from "@lottiefiles/react-lottie-player";
 import { motion } from "framer-motion";
+import { roboto } from "../../common/fonts";
 
 export const CardDesktop = () => {
   return (
-    <div className="mt-16 relative flex justify-center">
+    <div className="mt-16 flex justify-center relative">
       <motion.div
-        className="dark:bg-dark-tertiary/80 bg-light-tertiary/50 rounded-2xl w-fit flex items-center justify-center backdrop-blur-lg"
-        initial={{ opacity: 0, scale: 0.5 }} // Solo escalado inicial
+        className="dark:bg-dark-tertiary/80 bg-light-tertiary/50 rounded-2xl w-full max-w-[200px] backdrop-blur-lg z-10 dark:shadow-dark-subtle/50 shadow-sm shadow-light-title"
+        initial={{ opacity: 0, scale: 0.5, y: 0 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 0.8 }}
       >
+        <h3
+          className={`text-center ${roboto} font-semibold dark:text-dark-body text-light-body `}
+        >
+          Visualizá tus Tareas
+        </h3>
         <Player
           loop
           autoplay
           speed={0.5}
           src="/lottie/animation_3.json"
-          className="w-18 h-auto max-w-[200px]"
+          className="w-18 h-auto"
         />
       </motion.div>
-      
       <motion.div
-        className="dark:bg-dark-tertiary/80 bg-light-tertiary rounded-2xl w-fit flex items-center justify-center backdrop-blur-lg absolute left-1/4" // Posición a la izquierda
-        initial={{ opacity: 0, scale: 0.5, x: -30 }} // Escalado inicial
-        animate={{ opacity: 1, scale: 1, x: 0 }} // Se desplaza al centro
-        transition={{ duration: 0.8, delay: 0.8 }} // Se espera un momento para escalar
+        className="absolute left-1/2 transform -translate-x-full dark:bg-dark-tertiary/80 bg-light-tertiary rounded-2xl w-full max-w-[200px] backdrop-blur-lg z-0 dark:shadow-dark-subtle/50 shadow-sm shadow-light-title"
+        initial={{ opacity: 0, x: 0 }}
+        animate={{ opacity: 1, x: -360 }}
+        transition={{ duration: 0.8, delay: 4 }}
       >
+        <h3
+          className={`text-center ${roboto} font-semibold dark:text-dark-body text-light-body `}
+        >
+          Prioriza y Organizá
+        </h3>
         <Player
           loop
           autoplay
           speed={0.5}
           src="/lottie/animation_2.json"
-          className="w-18 h-auto max-w-[200px]"
+          className="w-18 h-auto"
         />
       </motion.div>
-      
       <motion.div
-        className="dark:bg-dark-tertiary/80 backdrop-blur-lg bg-light-tertiary rounded-2xl w-fit flex items-center justify-center absolute right-1/4" // Posición a la derecha
-        initial={{ opacity: 0, scale: 0.5, x: 30 }} // Escalado inicial
-        animate={{ opacity: 1, scale: 1, x: 0 }} // Se desplaza al centro
-        transition={{ duration: 0.8, delay: 0.8 }} // Se espera un momento para escalar
+        className="absolute left-1/2 transform translate-x-full dark:bg-dark-tertiary/80 bg-light-tertiary rounded-2xl w-full max-w-[200px] backdrop-blur-lg z-0 dark:shadow-dark-subtle/50 shadow-sm shadow-light-title"
+        initial={{ opacity: 0, x: 0, y: 10 }}
+        animate={{ opacity: 1, x: 160, y: 0 }}
+        transition={{ duration: 0.8, delay: 2 }}
       >
+        <h3
+          className={`text-center ${roboto} font-semibold dark:text-dark-body text-light-body `}
+        >
+          Colabora y Comparti
+        </h3>
         <Player
           loop
           autoplay
           speed={0.5}
           src="/lottie/animation_1.json"
-          className="w-18 h-auto stroke-black dark:stroke-white fill-black dark:fill-white max-w-[200px]"
+          className="w-18 h-auto stroke-black dark:stroke-white fill-black dark:fill-white"
         />
       </motion.div>
     </div>
