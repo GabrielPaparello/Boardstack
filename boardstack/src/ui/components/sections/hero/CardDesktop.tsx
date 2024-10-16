0bddf20e9c4c87480628f9664509f9b24bedf2a2
GabrielPaparello <69154223+GabrielPaparello@users.noreply.github.com>
2024-10-16 17:14:41 +0000

    fixeds

--- boardstack/src/ui/components/sections/hero/CardDesktop.tsx
+++ boardstack/src/ui/components/sections/hero/CardDesktop.tsx
@@ -5,9 +5,9 @@ import { roboto } from "../../common/fonts";
 
 export const CardDesktop = () => {
   return (
-    <div className="mt-16 flex justify-center relative">
+    <div className="mt-16 flex justify-center relative ">
       <motion.div
-        className="dark:bg-dark-tertiary/80 bg-light-tertiary/50 rounded-2xl w-full max-w-[200px] backdrop-blur-lg z-10 dark:shadow-dark-subtle/50 shadow-sm shadow-light-title"
+        className="dark:bg-dark-tertiary/80 bg-light-tertiary/50 rounded-2xl w-full max-w-[200px] backdrop-blur-lg z-10 dark:shadow-dark-subtle/50 shadow-sm shadow-light-title lg:max-w-[200px]  "
         initial={{ opacity: 0, scale: 0.5, y: 0 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 0.8 }}
@@ -22,11 +22,11 @@ export const CardDesktop = () => {
           autoplay
           speed={0.5}
           src="/lottie/animation_3.json"
-          className="w-18 h-auto lg:w-24 xl:w-32"
+          className="w-18 h-auto "
         />
       </motion.div>
       <motion.div
-        className="absolute left-1/2 transform -translate-x-full dark:bg-dark-tertiary/80 bg-light-tertiary rounded-2xl w-full max-w-[200px] backdrop-blur-lg z-0 dark:shadow-dark-subtle/50 shadow-sm shadow-light-title"
+        className="absolute left-1/2 transform -translate-x-full dark:bg-dark-tertiary/80 bg-light-tertiary rounded-2xl w-full max-w-[200px] backdrop-blur-lg z-0 dark:shadow-dark-subtle/50 shadow-sm shadow-light-title lg:max-w-[200px]  "
         initial={{ opacity: 0, x: 0 }}
         animate={{ opacity: 1, x: -360 }}
         transition={{ duration: 0.8, delay: 4 }}
@@ -41,11 +41,11 @@ export const CardDesktop = () => {
           autoplay
           speed={0.5}
           src="/lottie/animation_2.json"
-          className="w-18 h-auto lg:w-24 xl:w-32"
+          className="w-18 h-auto "
         />
       </motion.div>
       <motion.div
-        className="absolute left-1/2 transform translate-x-full dark:bg-dark-tertiary/80 bg-light-tertiary rounded-2xl w-full max-w-[200px] backdrop-blur-lg z-0 dark:shadow-dark-subtle/50 shadow-sm shadow-light-title"
+        className="absolute left-1/2 transform translate-x-full dark:bg-dark-tertiary/80 bg-light-tertiary rounded-2xl w-full max-w-[200px] lg:max-w-[200px]   backdrop-blur-lg z-0 dark:shadow-dark-subtle/50 shadow-sm shadow-light-title"
         initial={{ opacity: 0, x: 0, y: 10 }}
         animate={{ opacity: 1, x: 160, y: 0 }}
         transition={{ duration: 0.8, delay: 2 }}
@@ -60,7 +60,7 @@ export const CardDesktop = () => {
           autoplay
           speed={0.5}
           src="/lottie/animation_1.json"
-          className="w-18 h-auto stroke-black lg:w-24 xl:w-32 dark:stroke-white fill-black dark:fill-white"
+          className="w-18 h-auto stroke-black  dark:stroke-white fill-black dark:fill-white"
         />
       </motion.div>
     </div>
