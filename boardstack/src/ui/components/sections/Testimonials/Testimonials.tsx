58192e5c806fc555c35489d626a1475a3e3e2b9f
gabrielpaparello <69154223+GabrielPaparello@users.noreply.github.com>
2024-10-17 15:39:07 +0000

    background noise

--- boardstack/src/ui/components/sections/Testimonials/Testimonials.tsx
+++ boardstack/src/ui/components/sections/Testimonials/Testimonials.tsx
@@ -4,7 +4,8 @@ import { api } from "@/lib/data/testimonial.data";
 export const Testimonials = () => {
   return (
     <>
-      <div className="dark:bg-dark-tertiary bg-light-background dark:bg-dark-background   bg-light-tertiary flex flex-col lg:flex-row items-center justify-center gap-3 lg:gap-20 pt-7">
+      <div className="dark:bg-dark-background bg-light-background  relative     flex flex-col lg:flex-row items-center justify-center gap-3 lg:gap-20 pt-7">
+        <div className="absolute inset-0 dark:bg-dark-tertiary bg-light-tertiary opacity-70"></div>
         {api.map((data) => (
           <Card data={data} key={data.userName} />
         ))}
