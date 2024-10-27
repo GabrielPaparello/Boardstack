21af50c47f216b4848a990352acf61c7e2a39bcb
gabrielpaparello <69154223+GabrielPaparello@users.noreply.github.com>
2024-10-27 09:48:21 +0000

    restore blogtypes

--- boardstack/src/ui/types/blog.types.ts
+++ boardstack/src/ui/types/blog.types.ts
@@ -1,21 +1,21 @@
-// interface BlogType {
-//   featured: {
-//     id: string;
-//     title: string;
-//     subtitle: string;
-//     body: string;
-//     image: string;
-//     createdAt: string;
-//     updatedAt: string;
-//   };
-//   posts: Array<{
-//     id: string;
-//     title: string;
-//     subtitle: string;
-//     body: string;
-//     image: string;
-//     createdAt: string;
-//     updatedAt: string;
-//   }>;
-// }
-// // type OptionalBlogType = Partial<BlogType>;
+interface BlogType {
+  featured: {
+    id: string;
+    title: string;
+    subtitle: string;
+    body: string;
+    image: string;
+    createdAt: string;
+    updatedAt: string;
+  };
+  posts: Array<{
+    id: string;
+    title: string;
+    subtitle: string;
+    body: string;
+    image: string;
+    createdAt: string;
+    updatedAt: string;
+  }>;
+}
+type OptionalBlogType = Partial<BlogType>;
